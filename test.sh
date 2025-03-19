734426137769c710e02422b24921d39f041adb31
Ali Iftikhar <87130078+AliIfti@users.noreply.github.com>
2025-03-19 09:38:41 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Running tests..."
 # TODO: Add commands to run unit and integration tests
+#!/bin/bash
+set -e
+python -m unittest discover tests/
+echo "Tests executed successfully!"
